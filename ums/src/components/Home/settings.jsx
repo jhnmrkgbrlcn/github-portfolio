fd5ce86d1ac299ea95d0f03a5f32d9c2ce7cc5fa
VijayRico <bernardovijay9@gmail.com>
2023-11-16 22:33:59 +0800

    kape

--- ums/src/components/Home/settings.jsx
+++ ums/src/components/Home/settings.jsx
@@ -1,59 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
 
-const ChangePassword = () => {
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleChangePassword = () => {
-    // Perform password change logic here
-    if (currentPassword && newPassword && confirmPassword) {
-      if (newPassword === confirmPassword) {
-        // Implement your password change logic here
-        // For simplicity, let's just log the changes
-        console.log('Password changed successfully!');
-        setMessage('Password changed successfully!');
-      } else {
-        setMessage('New password and confirm password do not match.');
-      }
-    } else {
-      setMessage('Please fill in all fields.');
-    }
-  };
-
+const SettingsCard = () => {
   return (
-    <div>
-      <h2>Change Password</h2>
-      <div>
-        <label>Current Password:</label>
-        <input
-          type="password"
-          value={currentPassword}
-          onChange={(e) => setCurrentPassword(e.target.value)}
-        />
+    <div className="container mt-5 align-items-center">
+      <div className="card">
+        <div className="card-header">
+          <h5 className="card-title">Change Password</h5>
+        </div>
+        <div className="card-body">
+          <div className="form-group row-md-3">
+            <label className="form-label" htmlFor="currentPassword">
+              Current Admin Password:
+            </label>
+            <input
+              type="password"
+              id="currentPassword"
+              className="form-control form-control-lg"
+            />
+            <label className="form-label" htmlFor="newPassword">
+              New Admin Password:
+            </label>
+            <input
+              type="password"
+              id="newPassword"
+              className="form-control form-control-lg"
+            />
+            <label className="form-label" htmlFor="confirmPassword">
+              Confirm Admin Password:
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              className="form-control form-control-lg"
+            />
+          </div>
+
+          <div className="row">
+            <button className="btn btn-primary col-2">Save</button>
+            <button className="btn btn-primary col-2">Cancel</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <label>New Password:</label>
-        <input
-          type="password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Confirm Password:</label>
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleChangePassword}>Change Password</button>
-      <p>{message}</p>
     </div>
   );
 };
 
-export default ChangePassword;
-
+export default SettingsCard;
